a7175bbe365dadb2eea5cc023a112b719979284f
Nagarjuna G <76806891+nag-gd9@users.noreply.github.com>
2021-02-01 10:31:13 +0530

    Update settings.py

--- dev_mongo/settings.py
+++ dev_mongo/settings.py
@@ -136,18 +136,4 @@ MEDIA_ROOT  = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
-'''
-# AWS S3 Bucket
-AWS_ACCESS_KEY_ID = 'AKIAX2U5VNMYBSKY3YS4'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'djangopractice'
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_S3_SIGNATURE_VERSION = "s3v4"
-# AWS_S3_ENDPOINT_URL = 'https://s3.amazonaws.com'
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-'''
